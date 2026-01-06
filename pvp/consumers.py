d1ee1549be8bc8fcfdcc629572b91a5e11903476
Palenhame <111377258+Palenhame@users.noreply.github.com>
2026-01-06 20:54:42 +0300

    feat: Create PvpConsumer class; create consumers.py, routing.py; change asgi.py, settings.py

--- pvp/consumers.py
+++ pvp/consumers.py
@@ -1,7 +1,28 @@
+import json
 
-def main():
-    pass
-   
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-if __name__ == "__main__":
-    main()
+
+class PvpConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name = 'pvp'
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+        await self.send(
+            text_data=json.dumps(
+                    {'message': 'Соединение установленно'}
+            )
+        )
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
--- pvp/routing.py
+++ pvp/routing.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .consumers import PvpConsumer
+
 websocket_urlpatterns = [
-    path('ws/pvp/', )
+    path('ws/pvp/', PvpConsumer.as_asgi())
 ]
